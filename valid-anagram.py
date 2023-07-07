193e36cfd8c235cfcba897514610cf19fd396929
Suprotik Dutta <suprotikkvcdutta@gmail.com>
2023-07-07 23:44:45 +0530

    day-2

---
+++ valid-anagram.py
@@ -0,0 +1,20 @@
+def isAnagram(s: str, t: str) -> bool:
+    # Solution 2
+    if sorted(s) == sorted(t):
+        return True
+
+    # Solution 1
+    if len(s) != len(t):
+        return False
+    countS, countT = {}, {}
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+    return True
+
+
+a = isAnagram("anagram", "nagaram")
+print(a)
