3aa885ed215dbc4f525b7c4ab0136cf1f9e72434
Suprotik Dutta <suprotikkvcdutta@gmail.com>
2023-07-11 00:30:59 +0530

    diameter-of-a-bst

---
+++ diameter-of-a-bst.js
@@ -0,0 +1,25 @@
+//Optimized solution
+var diameterOfBinaryTree = function(root, max = [0]) {
+    diameterOfTree(root, max);
+
+    return max[0];
+};
+
+const diameterOfTree = (root, max) => {
+    const isBaseCase = root === null;
+    if (isBaseCase) return 0;
+
+    return dfs(root, max);
+}
+
+const dfs = (root, max) => {
+    const left = diameterOfTree(root.left, max);
+    const right = diameterOfTree(root.right, max);
+
+    const diameter = left + right;
+    max[0] = Math.max(max[0], diameter);
+
+    const height = Math.max(left, right);
+
+    return height + 1;
+}
