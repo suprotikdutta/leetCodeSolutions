971d3ae7799fa8c9f42bed44a19e5b8bc8895a77
Suprotik Dutta <suprotikkvcdutta@gmail.com>
2023-08-05 14:32:38 +0530

    car-fleet

---
+++ Stack/car-fleet.py
@@ -0,0 +1,25 @@
+#One Variable
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        fleet=0
+        maxTime=0
+        for p,s in pair:
+            time = (target-p)/s
+            if time>maxTime:
+                fleet+=1
+                maxTime = time
+        return fleet
+    
+#Using Stack
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
